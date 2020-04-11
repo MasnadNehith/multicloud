75c48e0abdd1a1015830441693e7536081233002
masnad <masnad.nehith@gmail.com>
2020-04-11 16:17:34 +0300

    header access control fix

--- fourth-deployment/aws/handler.js
+++ fourth-deployment/aws/handler.js
@@ -3,7 +3,14 @@ const {get, create} = require('multicloud-blog-api');
 const url = process.env.MONGODB;
 
 module.exports.status = async event => {
-  return { statusCode: 200, body: JSON.stringify('Status all good')};
+  return { 
+    statusCode: 200, 
+    body: JSON.stringify('Status all good'),
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Credentials' : true
+    }
+  };
 };
 
 module.exports.get = async event => {
@@ -12,11 +19,25 @@ module.exports.get = async event => {
     result : result,
     dataFrom : 'AWS'
   }
-  return { statusCode: 200, body: JSON.stringify(item)};
+  return { 
+    statusCode: 200, 
+    body: JSON.stringify(item),
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Credentials' : true
+    }
+  };
 };
 
 module.exports.create = async event => {
   const bodyInfo = JSON.parse(event.body);
   const result = await create(bodyInfo.header, bodyInfo.text, url);
-  return { statusCode: 200, body: JSON.stringify(result)};
+  return { 
+    statusCode: 200,
+     body: JSON.stringify(result),
+     headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Credentials' : true
+    }
+    };
 };
